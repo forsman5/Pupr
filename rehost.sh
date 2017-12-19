3a53d4ac351fe63623f8cd842d9bab0bd1cc045c
Joseph Forsman <jrforsman@gmail.com>
2017-12-19 16:04:31 -0500

    added automatic connection to rehost.ssh

--- rehost.sh
+++ rehost.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+ssh -tti ../challonge.pem ec2-user@ec2-18-216-242-170.us-east-2.compute.amazonaws.com << 'ENDSSH'
 pkill -f node
+cd petlandcatalog/
 git fetch --all
 git reset --hard origin/master
+rm -rf views
 mv Views views
 npm install
 node controller.js &
+sleep 5 #give time to let the server start up before exiting
+exit
+ENDSSH
