3ee582048110bb4fa9a08494da1ae07cc4ad1e4a
Joseph Forsman <jrforsman@gmail.com>
2018-01-01 16:56:04 -0500

    added comment count to /dogs/

--- Database/storedprocedures/GetDogs.sql
+++ Database/storedprocedures/GetDogs.sql
@@ -3,7 +3,20 @@ DELIMITER //
 CREATE PROCEDURE GetDogs()
 BEGIN
 
-SELECT c1.dogID, c1.name, c1.favorites, breed1, breed2
+SELECT outer1.dogID, name, favorites, comments, breed1, breed2
+FROM
+(SELECT Users_Dogs_comments.dogID, COUNT(comment) AS comments
+  FROM Users_Dogs_comments
+  GROUP BY dogID
+  UNION
+  SELECT Dogs.dogID, 0 AS comments
+  FROM Dogs
+  LEFT JOIN Users_Dogs_comments
+  ON Dogs.dogID = Users_Dogs_comments.dogID
+  WHERE Users_Dogs_comments.dogID IS NULL
+) AS outer1
+INNER JOIN
+(SELECT c1.dogID, c1.name, c1.favorites, breed1, breed2
 FROM (
   (SELECT dogID, name, favorites, breed AS breed1
    FROM Dogs
@@ -32,8 +45,9 @@ FROM (
   Breeds
   ON
   Breeds.breedID = d1.breedID
-)
-ORDER BY dogID;
+)) AS outer2
+ON outer1.dogID = outer2.dogID
+ORDER BY outer1.dogID;
 
 END //
 DELIMITER ;
