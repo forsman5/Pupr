9e4cc625fdfb75518bb29cdb1d12614d7855b519
Joseph Forsman <jrforsman@gmail.com>
2018-01-14 14:52:21 -0500

    began using loop for shell script

--- Database/storedprocedures/RefreshSprocs.sh
+++ Database/storedprocedures/RefreshSprocs.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "petland" < "CreateTables.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "petland" < "GetDog.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "petland" < "GetDogs.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "petland" < "GetSubmittedDogs.sql"
+for file in *.sql; do
+  mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "petland" < $file
+done
